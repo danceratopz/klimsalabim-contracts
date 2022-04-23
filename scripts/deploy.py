cc3bf7ec9dd9f05e377737387f83f7959115c53a
danceratopz <danceratopz@gmail.com>
2022-04-23 17:06:29 +0200

    chore: add mumbai deployment to deploy script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,7 +1,20 @@
 from brownie import accounts, network, KlimSalaBim
 
-def main():
+def deploy_dev():
     deploy_account = accounts[0]
     ksb_contract = KlimSalaBim.deploy({"from": deploy_account})
     print(ksb_contract)
     return ksb_contract
+
+def deploy_mumbai():
+    if network.show_active() != "polygon-test-alchemy":
+        print(f"ERROR: Incorrect network. Expected 'polygon-test', got {network.show_active}")
+        return
+    deploy_account = accounts.load("ksb_deployment_polygon_test")
+    ksb_contract = KlimSalaBim.deploy({"from": deploy_account}, publish_source=True)
+    print(ksb_contract)
+    return ksb_contract
+
+def main():
+    if network.show_active() == "polygon-test-alchemy":
+        deploy_mumbai()
