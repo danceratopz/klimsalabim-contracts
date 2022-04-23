d9b0c0b6e2b64440f9ad3237a2e859a70138d372
haurog <36535774+haurog@users.noreply.github.com>
2022-04-23 10:52:17 +0200

    autoformat and added entries to single travel struct

--- contracts/KlimSalaBim.sol
+++ contracts/KlimSalaBim.sol
@@ -3,47 +3,57 @@ pragma solidity 0.8.9;
 
 // import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-/// @title 
+/// @title
 /// @author danceratopz, haurog
-/// @notice 
+/// @notice
 //contract KlimSalaBim is IERC721Receiver {
 contract KlimSalaBim {
-
     uint256 eventId = 0;
-    
+
     enum TravelModes {
         Plane,
         Train,
-        Bus,        
+        Bus,
         Bike,
         Walk
     }
-    
+
     struct SingleCompensatedTravel {
+        string startingLocation;
         uint256 eventId;
         TravelModes modeOfTravel;
         uint256 distance;
+        uint256 toucanBadgeID;
     }
 
     mapping(address => SingleCompensatedTravel) public participants;
-    
+
     constructor() {}
 
-    /// @notice 
+    /// @notice
     /// @param startingLocation city
     /// @param distance in kilometers to event location
-    /// @dev 
-    /// @return 
+    /// @dev
+    /// @return
     function compensateSingleParticipantTravel(
-                                               string memory startingLocation,
-                                               uint256 distance,
-                                               TravelModes modeOfTravel) public returns (uint256) {
+        string memory startingLocation,
+        uint256 distance,
+        TravelModes modeOfTravel
+    ) public returns (uint256) {
+
+        // TODO: Connect to toucan retire function
+        // Send Matic to toucan protocol
+        // Receive Toucan ID
+
+        uint256 dummyToucanID = 1; // A dummy number to fill into struct TODO: needs to be the proper ID
 
         participants[msg.sender] = SingleCompensatedTravel({
-                                                           eventId: eventId,
-                                                           modeOfTravel: modeOfTravel,
-                                                           distance: distance
-                    });
+            startingLocation: startingLocation,
+            eventId: eventId,
+            modeOfTravel: modeOfTravel,
+            distance: distance,
+            toucanBadgeID: dummyToucanID
+        });
     }
 
     /* /// @dev Required to use safeTransferFrom() from OpenZeppelin's ERC721 contract (when transferring NFTs to this contract). */
@@ -59,5 +69,4 @@ contract KlimSalaBim {
     /*             keccak256("onERC721Received(address,address,uint256,bytes)") */
     /*         ); */
     /* } */
-    
 }
