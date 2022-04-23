2583b6a1bea586491d308654f99767d19fc58136
danceratopz <danceratopz@gmail.com>
2022-04-24 01:56:09 +0200

    chore: extend deploy script and test compensateSingleParticipantTravel

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,4 +1,24 @@
-from brownie import accounts, network, KlimSalaBim
+import json
+from brownie import accounts, network, Contract, KlimSalaBim
+
+# The path is relative to repo root (where brownie is executed)
+OFFSET_HELPERS_ABI_FILENAME = "./misc/abis/OffsetHelper.json"
+
+if 'test' in network.show_active():
+    ADDRESSES = {'nct': "0x7beCBA11618Ca63Ead5605DE235f6dD3b25c530E",
+                 'offset_helpers': "0x1A38e74D5190bA69938979aBe69ceb7b823209d3"}
+elif 'main' in network.show_active():
+    ADDRESSES = {'nct': "0xD838290e877E0188a4A44700463419ED96c16107",
+                 'offset_helpers': "0x79E63048B355F4FBa192c5b28687B852a5521b31"}
+else:
+    raise Exception(f"Unexpected network: network.show_active():")
+
+
+def load_offset_helpers_abi():
+    with open(OFFSET_HELPERS_ABI_FILENAME, 'r') as f:
+        abi = json.load(f)
+    return abi
+
 
 def deploy_fork():
     """
@@ -6,8 +26,14 @@ def deploy_fork():
     """
     deploy_account = accounts[0]
     ksb_contract = KlimSalaBim.deploy({"from": deploy_account})
-    print(ksb_contract)
-    return ksb_contract
+    print("Initialized KlimSalaBim contract:", ksb_contract)
+
+    offset_helpers_abi = load_offset_helpers_abi()
+    helpers_contract = Contract.from_abi("OffsetHelpers", ADDRESSES['offset_helpers'], offset_helpers_abi)
+    print("Initalized OffsetHelpers contract:", helpers_contract)
+
+    return ksb_contract, helpers_contract
+
 
 def deploy_mumbai():
     if network.show_active() != "polygon-test-alchemy":
@@ -18,10 +44,28 @@ def deploy_mumbai():
     print(ksb_contract)
     return ksb_contract
 
+
 def main():
+
     if 'fork' in network.show_active():
-        ksb_contract = deploy_fork()
-        return ksb_contract
+        ksb_contract, helpers_contract = deploy_fork()
+
+        # Calculate the required amount of MATIC to compenstate a specified amount of tco2
+        tco2_to_retire = int(1e16)
+        matic_required = helpers_contract.howMuchETHShouldISendToSwap(ADDRESSES['nct'], tco2_to_retire)
+        print("\n", matic_required/1e18, "MATIC required to offset", tco2_to_retire/1e18, "TCO2")
+
+        # Test ToucanProtocol's function in isolation:
+        # helpers_contract.autoOffset(NCT_CONTRACT_ADDRESS, tco2_to_retire, {'from': accounts[0], 'value': matic_required})
+
+        # Test the main KlimSalaBim compensation function:
+        ksb_contract.compensateSingleParticipantTravel("Zurich",
+                                                       100, # kilometers travelled
+                                                       tco2_to_retire,
+                                                       0,  # plane
+                                                       {'from': accounts[0], 'value': matic_required});
+        return ksb_contract, helpers_contract
+
     if network.show_active() == "polygon-test-alchemy":
         ksb_contract = deploy_mumbai()
-        return ksb_contract
+        return ksb_contract, helpers_contract
